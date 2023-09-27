b68c74785aa378edc8548161669f138560eb2b1b
Stuart MacGowan <smacgowan@dundee.ac.uk>
2023-09-27 02:19:23 +0100

    Add interactive variant table

--- dash-mol3d-demo.py
+++ dash-mol3d-demo.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dash_table, Input, Output, callback
+from dash import Dash, html, dash_table, Input, Output, callback, dcc
 import dash_bio as dashbio
 from dash_bio.utils import PdbParser, create_mol3d_style
 import dash_bootstrap_components as dbc
@@ -27,11 +27,22 @@ df = df[df['name'] == 'CA']
 df = df[['chain', 'residue_name', 'residue_index', 'positions']]
 
 # Load variant table from HDF5 file
-variants = pd.read_hdf('data/PF00104.29-swiss-varalign-tables.h5', key='variants')
-variants_columns = [('Alignment', 'Column'), ('Allele_INFO', 'AC'), ('Allele_INFO', 'AF_POPMAX')]
+variants = pd.read_hdf(
+    'data/PF00104.29-swiss-varalign-tables.h5', key='variants')
+variants_columns = variants.columns.tolist()
+# Drop problematic columns
+variants_columns.pop(97)  # ('Row', 'FILTER')
+variants_columns.pop(95)  # ('Row', 'ALT')
 variants = variants[variants_columns]
+
 # Flatten multi-level columns
 variants.columns = ['_'.join(col).strip() for col in variants.columns.values]
+
+# The column options for the dropdown
+column_options = [{'label': col, 'value': col} for col in variants.columns]
+
+# Default columns to display
+default_columns = variants.columns[:10].tolist()
 
 # Enhanced layout with Bootstrap
 navbar = dbc.Navbar(
@@ -42,8 +53,10 @@ navbar = dbc.Navbar(
             dbc.Collapse(
                 dbc.Nav(
                     [
-                        dbc.NavItem(dbc.NavLink("Home", href="#", id="home-link")),
-                        dbc.NavItem(dbc.NavLink("Results", href="#", id="results-link")),
+                        dbc.NavItem(dbc.NavLink(
+                            "Home", href="#", id="home-link")),
+                        dbc.NavItem(dbc.NavLink(
+                            "Results", href="#", id="results-link")),
                     ],
                     className="navbar-nav",
                 ),
@@ -126,22 +139,31 @@ app.layout = dbc.Container(
                     [
                         dbc.CardHeader("Variants"),
                         dbc.CardBody(
-                            dash_table.DataTable(
-                                id="zooming-specific-variants-table",
-                                columns=[{"name": i, "id": i}
-                                         for i in variants.columns],
-                                data=variants.to_dict("records"),
-                                page_size=10,
-                                style_table={
-                                    'overflowX': 'scroll',
-                                },
-                                style_cell={
-                                    'textAlign': 'left',
-                                    'font_family': 'Arial',
-                                    'font_size': '16px'
-                                },
-                            ),
+                            [
+                                dcc.Dropdown(
+                                    id='column-dropdown',
+                                    options=column_options,
+                                    value=default_columns,  # Default columns to display
+                                    multi=True
+                                ),
+                                dash_table.DataTable(
+                                    id="zooming-specific-variants-table",
+                                    columns=[{'name': i, 'id': i}
+                                             for i in default_columns],
+                                    data=variants.to_dict("records"),
+                                    page_size=10,
+                                    style_table={
+                                        'overflowX': 'scroll',
+                                    },
+                                    style_cell={
+                                        'textAlign': 'left',
+                                        'font_family': 'Arial',
+                                        'font_size': '16px'
+                                    },
+                                ),
+                            ],
                             className="variants-table-card-body"
+
                         ),
                     ],
                     className="variants-table-card"
@@ -181,6 +203,14 @@ def residue(selected_row):
             }
         ],
     ]
+
+@callback(
+    Output('zooming-specific-variants-table', 'columns'),
+    Input('column-dropdown', 'value'),
+    prevent_initial_call=True
+)
+def update_columns(selected_columns):
+    return [{'name': i, 'id': i} for i in selected_columns]
 
 
 if __name__ == "__main__":
